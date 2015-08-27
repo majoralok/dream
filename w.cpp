625ef95f16aaad61564e0663c79645b28e236fbb
majoralok <aloksinghaloksingh4444@gmail.com>
2015-08-27 23:51:03 +0530

    I added

---
+++ w.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int print(void);
+
+int main(void)
+{   //char a;
+   // operator>>(cin,a);   
+    //operator<<(cout,"hhe");
+    
+    
+    cout << "The Lucky " << print() << endl;     //This line
+    
+    
+    return 0;
+}
+
+int print(void)
+{
+    cout << "No : ";
+    return 2;
+}
